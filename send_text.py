6de605da249e59ada8f8276247df8f58082ab227
Gaurav <gameider12@gmail.com>
2019-01-02 11:49:33 -0500

    change text message

--- send_text.py
+++ send_text.py
@@ -5,7 +5,7 @@ account_sid = "AC32a3c49700934481addd5ce1659f04d2"
 auth_token  = "{{ auth_token }}"
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="Jenny please?! I love you <3",
+message = client.messages.create(body="Hey! What's up Steve?",
     to="+14159352345",    # Replace with your phone number
     from_="+14158141829") # Replace with your Twilio number
 print message.sid
